4240faeccd4d9bf94b8a671678f8a2acb296bf7d
Andrew Rost <andrewrost0@gmail.com>
2020-06-08 15:24:20 -0400

    Made basic Hello World and added testing images

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  templateUrl: '<h2> Hello World</h2>'
+  template: '<h2>Hello World</h2>'
 })
 export class EventsAppComponent {
   title = 'app';
