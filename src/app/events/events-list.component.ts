080bb575787b2f47219c5d3c771dd5bc42ed961b
Andrew Rost <andrewrost0@gmail.com>
2020-06-09 09:19:39 -0400

    added templates and more components

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'events-list',
+    templateUrl: './events-list.component.html'
+})
+
+export class EventsListComponent {
+    event = {
+        id:1,
+        name:'My First Event',
+        date: '6/9/20',
+        time:'10:00 AM',
+        price: 600.00,
+        imageUrl: 'assets/images/angularconnect-shield.png',
+        location: {
+            address: '226 East Church St',
+            city: 'Ligonier',
+            state: 'PA',
+            country: 'USA'
+
+        }
+    }
+
+}
